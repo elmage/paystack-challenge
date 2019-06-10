8b7c2ad8c531225ebd1207b83ea8c23d79a72502
Mage <mails4mage@gmail.com>
2019-06-10 15:43:04 +0100

    created migrations for suppliers and bank accounts

---
+++ app/Supplier/Supplier.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Supplier;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    //
+}
---
+++ database/migrations/2019_06_10_143302_create_bank_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('number')->unique();
+            $table->string('bank_code');
+            $table->string('bank_name');
+            $table->string('currency')->default('NGN');
+            $table->string('auth_code')->nullable();
+            $table->string('recipient_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
